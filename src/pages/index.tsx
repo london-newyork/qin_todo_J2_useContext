fb4f3fee3a95379a5a8e36626b62081023615615
london-newyork <cactus1272004@yahoo.co.jp>
2022-02-22 17:26:49 +0900

    #8【 UI 】ボタン「今日する」「明日する」「今度する」の実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { BtnArea } from "src/components/btn/BtnArea";
+import { SomeTimeBtn } from "src/components/btn/SomeTimeBtn";
+import { TodayBtn } from "src/components/btn/TodayBtn";
+import { TomorrowBtn } from "src/components/btn/TomorrowBtn";
 import { Layout } from "src/components/layout";
 
 const Home: NextPage = () => {
@@ -14,6 +18,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2 className="p-2 bg-red-400">Home</h2>
+      <BtnArea>
+        <TodayBtn />
+        <TomorrowBtn />
+        <SomeTimeBtn />
+      </BtnArea>
       <button onClick={handleClick}>Button</button>
     </Layout>
   );
