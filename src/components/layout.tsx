241e5154171c68686b483ae506854e6933a3cea7
geyageya <kageya@kaigai21.com>
2022-02-28 16:21:51 +0900

    💄 #36_input_task_desktop

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode, VFC } from "react";
+import { AddTask } from "src/components/addTask";
 import { Footer } from "src/components/footer";
 import { Header } from "src/components/header";
 
@@ -8,6 +9,7 @@ export const Layout: VFC<{ children: ReactNode }> = (props) => {
       <Header />
       <main>{props.children}</main>
       <Footer />
+      <AddTask />
     </>
   );
 };
