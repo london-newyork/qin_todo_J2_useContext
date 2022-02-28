efc9a34260215df9fe16458670c118c7b6bc9a17
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-01 00:50:48 +0900

    #35 ラジオボタンのトグル追加（色別まだ）

--- src/components/btn/RadioBtn/RadioBtn.tsx
+++ src/components/btn/RadioBtn/RadioBtn.tsx
@@ -4,59 +4,58 @@ import { useState } from "react";
 export const RadioBtn: VFC<{ children: ReactNode }> = (props) => {
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleChange = (e: any) => {
-    setIsChecked(e.target.value);
+  const handleChange = () => {
+    setIsChecked(!isChecked);
   };
 
   return (
     <div>
-      <label htmlFor="radio1">
+      <label>
         <input
           className="
-                    hidden
-                    "
-          id="radio1"
+        hidden
+        "
           name="radioBtn"
           type="radio"
+          checked={isChecked}
           onClick={handleChange}
-          checked
         />
         <span
           className={`
-                        relative
-                        box-border
-                        inline-block
-                        before:block
-                        after:block
-                        before:absolute
-                        after:absolute
-                        before:top-[45%]
-                        after:top-[45%]
-                        top-[4px]
-                        before:left-[5px]
-                        after:left-[9px]
-                        py-[30px]
-                        px-[5px]
-                        before:mr-[6px]
-                        w-auto
-                        before:w-6
-                        after:-mt-1
-                        after:w-4
-                        before:-mt-2
-                        before:h-6
-                        after:h-4
-                        before:content-['']
-                        after:content-['']
-                        before:bg-white
-                        before:rounded-full
-                        after:rounded-full
-                        before:border-2
-                        before:border-baseGray-200
-                        before:border-solid
-                        cursor-pointer
-                        pl-9
-                        ${isChecked ? "after:opacity-1 after:bg-primary-rose before:border-primary-rose" : ""}
-                `}
+        relative
+        box-border
+        inline-block
+        before:block
+        after:block
+        before:absolute
+        after:absolute
+        before:top-[45%]
+        after:top-[45%]
+        top-[4px]
+        before:left-[5px]
+        after:left-[9px]
+        py-[30px]
+        px-[5px]
+        before:mr-[6px]
+        w-auto
+        before:w-6
+        after:-mt-1
+        after:w-4
+        before:-mt-2
+        before:h-6
+        after:h-4
+        before:content-['']
+        after:content-['']
+        before:bg-white
+        before:rounded-full
+        after:rounded-full
+        before:border-2
+        before:border-baseGray-200
+        before:border-solid
+        cursor-pointer
+        pl-9
+        ${isChecked ? "after:opacity-1 after:bg-primary-rose before:border-primary-rose" : "after:opacity-0"}
+        `}
         >
           {props.children}
         </span>
