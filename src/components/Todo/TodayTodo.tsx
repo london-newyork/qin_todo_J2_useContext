fed1eadd430329622f971d9bd3ac8b7c80065adb
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-11 01:17:21 +0900

    #48 WIP タスク追加　エンターキーとstate管理
    エンターキーで下へタスクが追加されるように設定中

--- src/components/Todo/TodayTodo.tsx
+++ src/components/Todo/TodayTodo.tsx
@@ -1,16 +1,66 @@
 import type { VFC } from "react";
+import { useState } from "react";
 import { RadioBtn } from "src/components/btn/RadioBtn/RadioBtn";
 import { RadioBtnGroup } from "src/components/btn/RadioBtn/RadioBtnGroup";
 import { NewTask } from "src/components/NewTask";
 import { TodayTitle } from "src/components/Title/TodayTitle";
 
+type Task = {
+  readonly id: string;
+  index: number;
+  array: string[];
+  value: string;
+};
+
 export const TodayTodo: VFC = () => {
+  const [todayTask, setTodayTask] = useState<Task[]>([]);
+
+  const handleChangeTodayTask = (e: string | any) => {
+    setTodayTask(() => {
+      return e.target.value;
+    });
+  };
+
+  //下へmapで追加
+  // const AddNewTasks = () => {
+
+  //   }
+
+  const handleOnKeyPress = (e: any) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      //下へmapで追加
+
+      setTodayTask(() => {
+        return e.target.value;
+      });
+    }
+  };
+
   return (
     <div className="flex-1 w-full">
       <TodayTitle />
       <RadioBtnGroup>
         <RadioBtn variant="rose" value="task1">
-          Next.jsをインストール
+          <ul>
+            {/* <li>
+              <input
+                value={todayTask}
+                onChange={handleChangeTodayTask}
+                onKeyPress={handleOnKeyPress}
+              />
+            </li> */}
+            {/* !prevTask ? */}
+            {todayTask
+              ? todayTask.map((task: Task) => {
+                  return (
+                    <li key={task.id}>
+                      <input value={todayTask} onChange={handleChangeTodayTask} onKeyPress={handleOnKeyPress} />
+                    </li>
+                  );
+                })
+              : undefined}
+          </ul>
         </RadioBtn>
       </RadioBtnGroup>
       <NewTask />
