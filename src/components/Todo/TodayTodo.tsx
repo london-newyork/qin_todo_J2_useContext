cb2df1d0c7355e86a8b83b98e051842e58b3c52f
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-12 00:34:49 +0900

    #48 stateの更新関数のオブジェクト内にidを追加
    配列を回すため。ただしタスクの中身に文字列がなぜか入らない。

--- src/components/Todo/TodayTodo.tsx
+++ src/components/Todo/TodayTodo.tsx
@@ -1,4 +1,4 @@
-import type { VFC } from "react";
+import type { SetStateAction, VFC } from "react";
 import { useCallback } from "react";
 import { useState } from "react";
 import { RadioBtn } from "src/components/btn/RadioBtn/RadioBtn";
@@ -9,62 +9,62 @@ import { TodayTitle } from "src/components/Title/TodayTitle";
 type Task = {
   readonly id: string;
   index: number;
-  array: string[];
-  value: string;
+  task?: string;
 };
 
 export const TodayTodo: VFC = () => {
-  const [todayTask, setTodayTask] = useState<Task[]>([]);
-
-  const handleChangeTodayTask = (e: string | any) => {
-    setTodayTask(() => {
-      return [e.target.value];
-    });
-  };
-
-  // console.log(todayTask);
+  const [todayTask, setTodayTask] = useState<SetStateAction<string[] | Task[]>>([""]);
+  const [isOnMouse] = useState<boolean>(false);
 
   const getUniqueId = () => {
     return new Date().getTime().toString(36) + "-" + Math.random().toString(36);
   };
-
   const id = getUniqueId();
+
+  const handleChangeTodayTask = useCallback(
+    (e: string | any) => {
+      const AddNewTasks = [
+        {
+          id,
+          task: e.target.value,
+        },
+        ...todayTask,
+      ];
+      setTodayTask(AddNewTasks);
+    },
+    [todayTask]
+  );
+
+  // console.log(todayTask);
 
   const handleOnKeyDown = useCallback(
     (e: any) => {
-      if (e.key == "Enter") {
+      if (e.key === "Enter") {
         e.preventDefault();
-
-        const AddNewTasks = [e.target.value, ...todayTask];
-        setTodayTask(AddNewTasks);
+        const EnteredTask = [
+          {
+            id,
+            task: e.target.value,
+          },
+          ...todayTask,
+        ];
+        setTodayTask(() => {
+          return EnteredTask;
+        });
       }
       return;
     },
-    [todayTask]
+    [id, todayTask]
   );
 
   return (
     <div className="flex-1 w-full">
       <TodayTitle />
       {/* <NewTask /> */}
-      <RadioBtnGroup key={id}>
-        <RadioBtn variant="rose" value="task1">
-          <input
-            placeholder="タスクを追加する"
-            value={todayTask}
-            onChange={handleChangeTodayTask}
-            onKeyDown={handleOnKeyDown}
-            className="
-              focus:outline-none
-              caret-[#F43F5E]
-              "
-          />
-        </RadioBtn>
-      </RadioBtnGroup>
-      {todayTask
-        ? todayTask.map(() => {
+      {!isOnMouse
+        ? todayTask.map((task: Task) => {
             return (
-              <RadioBtnGroup key={id}>
+              <RadioBtnGroup key={task.id}>
                 <RadioBtn variant="rose" value="task1">
                   <input
                     placeholder="タスクを追加する"
@@ -80,7 +80,7 @@ export const TodayTodo: VFC = () => {
               </RadioBtnGroup>
             );
           })
-        : undefined}
+        : null}
     </div>
   );
 };
