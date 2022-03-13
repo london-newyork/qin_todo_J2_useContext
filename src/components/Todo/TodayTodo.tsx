fc4394640a299bd85dc3d674b00db5219cc307e2
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-14 01:22:28 +0900

    #48 仕様変更のためinputタグをtextareaに変更
    - 空文字の時にリストが追加できない処理が書けなかった
    - 最大200文字まで書けるようにしたがテキストエリアが縦に広がらなくなった

--- src/components/Todo/TodayTodo.tsx
+++ src/components/Todo/TodayTodo.tsx
@@ -28,6 +28,12 @@ export const TodayTodo: VFC = () => {
       //再レンダリング防止処理
       if (renderFlgRef.current) {
         renderFlgRef.current = false;
+        //  const newText = e.target.value.replace(/\n/g, "")
+        // if (newText.length > 200) {
+        //   alert('入力可能な文字数を超えています。')
+        //   return
+        // }
+        //   setText(newText);
         e.target.value;
       }
       return;
@@ -37,15 +43,27 @@ export const TodayTodo: VFC = () => {
 
   // console.log(todayTask);
 
-  const handleChangeIcon = () => {
-    // console.log('clickされたよ');
-  };
+  //最大200文字まで書き込み、それ以上は入力文字数制限
+  // const handleCountChange = (e: any) => {
+  //     const truncate = (str:string, length:number) => {
+  //       return str.length <= length ? str: alert('入力可能な文字数を超えています。');
+  //     }
+  //     truncate(e.target.value.length, 200)
+  // }
 
   const handleOnKeyDown = useCallback(
     (e: any) => {
+      //ここにリスト一番上に新しく入力欄が出現する処理をかく
+      //何も入力せずBlurしてもリストが作成できないようにする
+
       const task = e.target.value;
-      if (e.key === "Enter") {
-        e.preventDefault();
+      //エンター１回だと改行、2回で下にリスト追加され、
+      //何も入力せずにエンターしてもリストが作成できないようにする
+      // if (Object.keys(todayTask).length && e.key === "Enter") {
+      //   return
+      // }
+      if (task && e.key === "Enter") {
+        // e.preventDefault();
         const EnteredTask = [
           {
             id,
@@ -61,8 +79,8 @@ export const TodayTodo: VFC = () => {
           // return EnteredTask.sort((a,b)=>{return b[0] - a[0]})
           return EnteredTask;
         });
-        // console.log('エンターキーです');
       }
+
       return;
     },
     [id, todayTask]
@@ -71,21 +89,25 @@ export const TodayTodo: VFC = () => {
   return (
     <div className="flex-1 w-full">
       <TodayTitle />
+      {/* 配列の逆転ループする処理をここに書く。 */}
       {Object.keys(todayTask).length
         ? todayTask.map((item: Task) => {
             return (
               <RadioBtnGroup key={item.id}>
                 {!Object.keys(todayTask).length ? <PlusBtn /> : <RadioBtn variant="rose" value="task1" />}
-                <input
+                <textarea
                   placeholder="タスクを追加する"
                   value={item.task}
-                  onClick={handleChangeIcon}
+                  // onKeyUp={handleCountChange}
+                  maxLength={200}
                   onChange={handleChangeTodayTask}
                   onKeyDown={handleOnKeyDown}
                   className="
-                  pt-1
+                  overflow-hidden
+                  mt-3
                   focus:outline-none
                   caret-[#F43F5E]
+                  resize-none
                   "
                 />
               </RadioBtnGroup>
