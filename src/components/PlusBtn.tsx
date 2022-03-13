a50d73df182fd77c5c248a9812741b2540f0287e
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-13 22:36:31 +0900

    #48 PlusBtnリファクタとRadioBtnGroupのcss一部削除
    - PlusBtnとラジオボタンを切り替えて使うために位置など修正しており、以前の記述を部分的に削除
    - RadioBtnGroupにリスト反転させるためのCSSを書いていたが意味がなかったので削除

--- src/components/PlusBtn.tsx
+++ src/components/PlusBtn.tsx
@@ -8,10 +8,8 @@ import type { VFC } from "react";
 export const PlusBtn: VFC = () => {
   return (
     <div>
-      {/* <div className="flex relative flex-row items-center p-2 w-[180px] h-[40px] "> */}
       <div className="relative py-2 pr-3 mt-1">
         <div className="box-border flex justify-center items-center w-[22px] h-[22px] bg-[#C2C6D2] rounded-full border-solid cursor-pointer">
-          {/* <div className="box-border flex absolute justify-center items-center w-[22px] h-[22px] bg-[#C2C6D2] rounded-full border-solid cursor-pointer"> */}
           <Image alt="plus" src="/plus_icon.png" width="11px" height="11px" />
         </div>
       </div>
