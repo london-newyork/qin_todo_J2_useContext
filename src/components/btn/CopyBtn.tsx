dfdfd8d7ceaf3a9a3cdbdf134e552810d6b7445e
london-newyork <cactus1272004@yahoo.co.jp>
2022-03-21 23:45:20 +0900

    #50 複製ボタンのリファクタ
    レビューを受けてリファクタした。

--- src/components/btn/CopyBtn.tsx
+++ src/components/btn/CopyBtn.tsx
@@ -11,26 +11,21 @@ type TodoItemProps = {
 };
 
 export const CopyBtn: VFC<TodoItemProps> = (props) => {
+  //  console.log('初期のProps: ',props);
   const getUniqueId = () => {
     return new Date().getTime().toString(36) + "-" + Math.random().toString(36);
   };
 
-  const handleDuplicate = (e: any) => {
-    e.preventDefault();
-
+  const handleDuplicate = () => {
     //複製したリストのために新しくIDを取得
     const newId = getUniqueId();
     props.setTaskList((prev) => {
-      //コピー元のリストのすぐ下へコピーしたオブジェクトを入れるため、indexを指定して入れる
-      const index = prev.findIndex((t) => {
-        return t.id === props.id;
+      const filteredItem = prev.filter((item) => {
+        item.id === props.id;
       });
-      prev.splice(index, 0, { id: newId, task: props.task });
-
-      return [...prev];
+      return filteredItem.concat({ id: newId, task: props.task }, ...prev);
     });
   };
-
   return (
     <button onClick={handleDuplicate}>
       <DocumentDuplicateIcon className="w-5 h-5 text-baseGray-200 opacity-0 group-hover:opacity-100" />
