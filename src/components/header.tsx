950df68f42e80a07466a9f7418a66c1406cb42fb
ikk2kk <ikk2kk@icloud.com>
2022-03-22 15:49:49 +0900

    ✨ #56 PopoverのUIを作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 // import Link from "next/link";
+import { CogIcon, LogoutIcon } from "@heroicons/react/outline";
+import { Popover, Text } from "@mantine/core";
 import Image from "next/image";
 import type { VFC } from "react";
+import { useState } from "react";
 
 // const items = [
 //   { href: "/", label: "Home" },
@@ -8,6 +11,26 @@ import type { VFC } from "react";
 // ];
 
 export const Header: VFC = () => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handlePopoverClose = () => {
+    return setIsOpened(false);
+  };
+
+  const handleImageClick = () => {
+    return setIsOpened((o) => {
+      return !o;
+    });
+  };
+  const handleSetting = () => {
+    alert("setting");
+    handlePopoverClose();
+  };
+
+  const handleLogout = () => {
+    alert("login");
+    handlePopoverClose();
+  };
   return (
     <header
       className="
@@ -70,17 +93,54 @@ export const Header: VFC = () => {
             sm:pr-0
             "
           >
-            <Image
-              className="object-cover rounded-full"
-              src={`/shimabu_icon.jpeg`}
-              alt="account_image"
-              width={36}
-              height={36}
-              quality={75} // 画質, number
-              priority={false} // 表示の優先度, boolean
-              loading={"lazy"} // 遅延ロードするかどうか, "lazy" | "eager"
-              unoptimized={false} // 最適化するかどうか, boolean
-            />
+            <Popover
+              opened={isOpened}
+              onClose={handlePopoverClose}
+              target={
+                <Image
+                  className="object-cover rounded-full"
+                  src={`/shimabu_icon.jpeg`}
+                  alt="account_image"
+                  width={36}
+                  height={36}
+                  quality={75} // 画質, number
+                  priority={false} // 表示の優先度, boolean
+                  loading={"lazy"} // 遅延ロードするかどうか, "lazy" | "eager"
+                  unoptimized={false} // 最適化するかどうか, boolean
+                  onClick={handleImageClick}
+                />
+              }
+              width={340}
+              position="bottom"
+              classNames={{
+                wrapper: "rounded-2xl focus:ring-0",
+                popover: "rounded-2xl",
+                inner: "px-0  rounded-2xl",
+                body: "rounded-2xl",
+              }}
+              // withArrow
+            >
+              <div className="flex flex-col">
+                <button
+                  className="flex items-center py-2 px-6 space-x-3 font-bold hover:bg-gray-100 outline-none hover:cursor-pointer"
+                  onClick={handleSetting}
+                >
+                  <CogIcon className="w-6 h-6 font-bold" />
+                  <Text className="hover:bg-gray-100" size="sm">
+                    設定
+                  </Text>
+                </button>
+                <button
+                  className="flex items-center py-2 px-6 space-x-3 font-bold text-red-500 hover:bg-gray-100 outline-none hover:cursor-pointer"
+                  onClick={handleLogout}
+                >
+                  <LogoutIcon className="w-6 h-6 font-bold" />
+                  <Text className="hover:bg-gray-100 " size="sm">
+                    ログアウト
+                  </Text>
+                </button>
+              </div>
+            </Popover>
           </div>
         </div>
       </nav>
